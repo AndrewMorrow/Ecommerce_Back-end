c1e9e54f8e0f098770496cbf18749ef4b512a312
AndrewMorrow <beardedmongo@gmail.com>
2020-12-12 14:21:50 -0600

    seeds fixed and product routes finished

--- models/Product.js
+++ models/Product.js
@@ -8,38 +8,38 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-    {
-        product_name: { type: DataTypes.STRING, allowNull: false },
-        price: {
-            type: DataTypes.DECIMAL,
-            allowNull: false,
-            validate: {
-                isDecimal: true,
-            },
-        },
-        stock: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 10,
-            validate: {
-                isNumeric: true,
-            },
-        },
-        category_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "Category",
-                key: "id",
-            },
-        },
+  {
+    product_name: { type: DataTypes.STRING, allowNull: false },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "product",
-    }
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product",
+  }
 );
 
 module.exports = Product;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,29 +5,35 @@ const sequelize = require("../config/connection");
 class ProductTag extends Model {}
 
 ProductTag.init(
-    {
-        product_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "Product",
-                key: "id",
-            },
-        },
-        tag_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "Tag",
-                key: "id",
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "product_tag",
-    }
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product_tag",
+  }
 );
 
 module.exports = ProductTag;
